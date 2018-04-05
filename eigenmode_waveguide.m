36a7132a9d32e2294c3ba2ab9d21f34d32757931
Yifan Wang <yfwang.star@gmail.com>
2018-04-05 00:00:21 -0400

    04-04 TM modes

---
+++ eigenmode_waveguide.m
@@ -0,0 +1,74 @@
+close all; clear all; clc;
+
+load('model1/mesh.mat');
+% https://www.mathworks.com/help/pde/ug/pde.pdemodel.generatemesh.html
+% https://www.mathworks.com/help/pde/ug/mesh-data.html
+% e(6,k) is the subdomain number on the left side of the edge (subdomain 0 is the exterior of the geometry), where direction along the edge is given by increasing parameter values.
+% e(7,k) is the subdomain number on the right side of the edge.
+
+t = t(1:3,:);
+b_e = e(1:2,:);
+
+tNum = size(t,2);
+b_eNum = size(b_e,2);
+pNum = size(p,2);
+
+all_n = [1:pNum].';
+edge_n = unique(b_e(:));
+int_n = setdiff(all_n, edge_n);
+
+N = size(int_n,1);
+int_or_edge = ones(pNum, 1);
+int_or_edge(edge_n) = 0;
+int_or_edge = logical(int_or_edge);
+
+% A = sparse(N, N);
+% B = sparse(N, N);
+A = sparse(pNum, pNum);
+B = sparse(pNum, pNum);
+
+for ii = 1:tNum
+    node = t(1:3, ii);
+    % int_node_flag = int_or_edge(node);
+    % int_node = node(int_node_flag);
+    [AA, BB] = get_AB(p(:,node));
+    A(node, node) = A(node, node) + AA;
+    B(node, node) = B(node, node) + BB;
+end
+
+nnz(A)
+nnz(A(int_n, int_n))
+figure(3);imagesc(full(A)~=0);colorbar;axis('image');
+figure(4);imagesc(full(B)~=0);colorbar;axis('image');
+% e=eig(A(int_n, int_n), B(int_n, int_n))
+[V,D,flag] = eigs(A(int_n, int_n), B(int_n, int_n), 10, 'sm'); % 2017a
+
+figure(2);
+for eigen_ii=1:4
+    subplot(2,2,eigen_ii);
+    Ez_all = zeros(pNum, 1);
+    Ez_all(int_n) = V(:,eigen_ii);
+    trisurf(t.',p(1,:).',p(2,:).',Ez_all);
+    view(2);xlabel('x'); ylabel('y'); zlabel('E_z');axis('equal');
+end
+
+% e=eigs(A(int_n, int_n), B(int_n, int_n), 10, 'smallestabs') 
+% 2017b + https://www.mathworks.com/help/matlab/ref/eigs.html#bu2_q3e-sigma
+% eigs(A,k,'smallestabs') returns the k smallest magnitude eigenvalues.
+
+
+figure(1);
+subplot(1,3,1);
+triplot(t.',p(1,:).',p(2,:).'); % TRI m x 3, x, y
+xlabel('x'); ylabel('y'); axis('equal');
+
+subplot(1,3,2);
+x1_b_e = p(1,b_e(1,:));
+x2_b_e = p(1,b_e(2,:));
+y1_b_e = p(2,b_e(1,:));
+y2_b_e = p(2,b_e(2,:));
+x_b_e = [x1_b_e;x2_b_e;nan(1,b_eNum)];
+y_b_e = [y1_b_e;y2_b_e;nan(1,b_eNum)];
+plot(x_b_e(:), y_b_e(:), 'k.-');
+xlabel('x'); ylabel('y'); axis('equal');
+clear('x1_b_e','x2_b_e','y1_b_e','y2_b_e','x_b_e','y_b_e');
---
+++ readme.md
@@ -0,0 +1,7 @@
+04/04/2018
+
+following 9.2.3 in Theory and computation of electromagnetic fields - 2ed - Jian-Ming Jin.pdf
+get the first few TM modes
+
+TODO
+[] figure out the eigenvalues k_c ^ 2 = 10^4 * [ 0.4120    0.6353    1.3346    1.0153 ... ]
